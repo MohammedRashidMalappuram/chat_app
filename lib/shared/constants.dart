ef1a952b6e9c1c326a8e44a92b515907f1538252
Mohammed Rashid <rashidak1022@gmail.com>
2023-04-25 08:58:28 +0530

    First commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String apiKey = "AIzaSyDHs4tR4ta4KutytZp-xzbVVG258hDjOfg";
+  static String appId = "1:738724397625:web:f1bdf8822e62ea05c403e8";
+  static String messagingSenderId = "738724397625";
+  static String projectId = "chatappflutter-ad713";
+}
