03adbad3056e25b3c27b82d187d4aa6245bfb5b7
Mohammed Rashid <rashidak1022@gmail.com>
2023-05-06 22:23:56 +0530

    LoginPageCompleteUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/helper/helper_function.dart';
+import 'package:chat_app/pages/auth/login_page.dart';
+import 'package:chat_app/pages/home_page.dart';
 import 'package:chat_app/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
@@ -17,16 +20,40 @@ void main() async {
     await Firebase.initializeApp();
   }
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+          primaryColor: Constants().primaryColor,
+          scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
   }
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static String apiKey = "AIzaSyDHs4tR4ta4KutytZp-xzbVVG258hDjOfg";
   static String appId = "1:738724397625:web:f1bdf8822e62ea05c403e8";
   static String messagingSenderId = "738724397625";
   static String projectId = "chatappflutter-ad713";
+  final primaryColor = const Color(0xFFee7b64);
 }
